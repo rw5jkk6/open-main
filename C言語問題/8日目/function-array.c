571503b422eebcd6a5e9dc3f39f0acc0877b7288
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-07-24 21:12:37 +0900

    Update function-array.c

--- C言語問題/8日目/function-array.c
+++ C言語問題/8日目/function-array.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
-#define NELEMS(a) (sizeof(a)/ sizeof((a)[0]))
+#define NELEMS(a) (sizeof(a) / sizeof((a)[0]))
+
+void add1(int [], int);
 
 void add1(int a[], int n){
     int i;
 
     for (i = 0; i < n; i++){
         a[i]++;
+    return;
     }
-    return ;
 }
 
 int main(){
-    int a[] = {1, 2, 3, 4, 5};
+    int a[]={1, 2, 3, 4, 5};
     int i;
 
-    for (i =0; i < NELEMS(a); i++){
+    // 下のを消すと 1, 2, 3, 4, 5
+    add1(a, NELEMS(a)); 
+
+    for (i = 0; i < NELEMS(a); i++)
         printf("%d ", a[i]);
-    }
     printf("\n");
+    return 0;
 }
+
+// 2 2 3 4 5
