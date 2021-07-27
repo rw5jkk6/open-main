6befe8f7cd053d4495a358fd9efe64a949130f5a
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-07-27 19:17:24 +0900

    Update cat.c

--- C言語問題/8日目/cat.c
+++ C言語問題/8日目/cat.c
@@ -1,8 +1,18 @@
+/*
+コマンドライン引数に与えないか、与えるかを条件分岐で分けている
+
+C言語では ctrl + d で EOF(int型) を標準入力する 
+*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void type_one_file(FILE *fp){
     int ch;
+    
+    // cf287のファイルを参照
+    // int putchar(int)
     while ((ch = getc(fp)) != EOF){
         putchar(ch);
     }
@@ -10,6 +20,7 @@ void type_one_file(FILE *fp){
 
 int main(int argc, char **argv){
     if (argc == 1){
+        // FILE *stdin
         type_one_file(stdin);
     }else{
         int i;
@@ -17,6 +28,8 @@ int main(int argc, char **argv){
 
         for (i =1; i <argc; i++){
             fp = fopen(argv[i], "rb");
+            
+            // エラー処理
             if (fp == NULL){
                 fprintf(stderr, "%s:%s can not open.\n", argv[0], argv[i]);
                 exit(1);
