8a012c3e8111d3d794ce709957be59ba1ec62b03
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-03 23:02:27 +0900

    Update 409.c

--- Go言語/C言語の基礎/4日目/409.c
+++ Go言語/C言語の基礎/4日目/409.c
@@ -1,1 +1,13 @@
-dddd
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+func main(){
+	rand.Seed(time.Now().UnixNano())
+	a := rand.Intn(10)
+	b := rand.Intn(10)
+	fmt.Printf("%d + %d = %d\n", a, b, a+b)
+}
