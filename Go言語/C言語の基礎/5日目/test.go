65d647356258adaad1b58985433577e0fcb127ae
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-04 16:28:02 +0900

    Add files via upload

---
+++ Go言語/C言語の基礎/5日目/test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	
+	s  := "ABC"
+	// 文字列のコピー
+	ss := s
+	fmt.Println(ss)
+
+	// 文字列の結合
+	ss = s + "DEF"
+	fmt.Println(ss)
+
+	// 文字の数　これだけ関数
+	le := len(ss)
+	fmt.Printf("文字列の長さ:%d\n",le)
+}
