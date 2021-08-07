2bea2419c813d27e99acbd9924da6f80415402ed
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-07 18:39:46 +0900

    Update and rename value-function.go to nil.go

---
+++ Go言語/Goの文法/NULL-nil/nil.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	var i int
+	fmt.Println(i)  // 0
+
+	var ii *int
+	fmt.Println(ii)  // <nil>
+
+	var n [2]int      
+	fmt.Println(n)   // [0,0]
+	
+	var nn *[2]int
+	fmt.Println(nn)   // <nil>
+
+	var m []int      
+	fmt.Println(m)   // []
+	
+	var mm *[]int
+	fmt.Println(mm)   // <nil>
+
+	var a interface{}
+	fmt.Println(a)   // <nil>
+
+	var aa *interface{}
+	fmt.Println(aa)  // <nil>
+}
