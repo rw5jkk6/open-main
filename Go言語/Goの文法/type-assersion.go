c5af430585ba789689bc4c130d06a4925b52808e
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-08 18:33:00 +0900

    Create type-assersion.go

---
+++ Go言語/Goの文法/type-assersion.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func do(i interface{}){
+   switch v := i.(type){
+   case int:
+      fmt.Println(v * 2)
+   case string:
+      fmt.Println(v + "!")
+   default:
+      fmt.Printf("I don't know %T\n", v)
+   }
+}
+
+func main(){
+    do(4)
+    do("Hello")
+    do(true)
+}
