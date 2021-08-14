dbd9be5621709c850c138828172ffc9d027dce53
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-14 13:44:53 +0900

    Update test.go

--- Go言語/C言語の基礎/7日目/702/test.go
+++ Go言語/C言語の基礎/7日目/702/test.go
@@ -2,40 +2,14 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io/ioutil"
+	"log"
 )
 
 func main(){
-	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Millisecond)
-	for{
-		select{
-		case <-tick:
-			fmt.Println("tick")
-		case <-boom:
-			fmt.Println("Boom")
-			return 
-		default:
-			fmt.Println("   ")
-			// 下の消すとスペースが延々と続く
-			time.Sleep(50 * time.Millisecond)
-		}
-	}
+   content, err := ioutil.ReadFile("test.txt")
+   if err != nil{
+      log.Fatal(err)
+   }
+   fmt.Println(string(content))
 }
-
-//   
-//   
-// tick
-   
-   
-// tick
-   
-   
-// tick
-   
-   
-// tick
-   
-   
-// tick
-// Boom
