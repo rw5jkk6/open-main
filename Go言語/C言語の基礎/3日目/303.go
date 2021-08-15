ae28c481e96f2c28cef0c28786f3e267569959b3
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-15 21:44:15 +0900

    Create 303.go

---
+++ Go言語/C言語の基礎/3日目/303.go
@@ -0,0 +1,15 @@
+// cにはwhileがない
+
+// #include <stdio.h>
+ 
+// int main(void){
+//     int i = 0;
+//     while (i <= 5){
+//         printf("%d ", i);
+//         i++;
+//     }
+//     printf("\n");
+//     return 0;
+// }
+
+// 1 2 3 4 5
