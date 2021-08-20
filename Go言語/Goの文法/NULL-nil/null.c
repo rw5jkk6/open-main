d043e77108ef1595afa28f3f256114c9c1c087b7
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-20 16:15:32 +0900

    Update null.c

--- Go言語/Goの文法/NULL-nil/null.c
+++ Go言語/Goの文法/NULL-nil/null.c
@@ -7,4 +7,10 @@ int main(void){
    
    int *ii;
    printf("%p\n", ii);    //  0x7ffee0eb2780
+   printf("%d\n", *ii);   // 0
+
+   int *iii=NULL;
+   printf("%p\n", iii);   // 0x0
+   printf("%d\n", *iii);  // Segmentation fault: 11
+   
 }
