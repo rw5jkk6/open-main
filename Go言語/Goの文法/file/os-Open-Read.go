894ce7dfb8a91f9e4596b6cc19d0193700f39a18
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-08-21 19:49:39 +0900

    Update and rename range-string-array.go to os-Open-Read.go

---
+++ Go言語/Goの文法/file/os-Open-Read.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main(){
+   f, err := os.Open("test.txt")
+   if  err != nil{
+      fmt.Println(err)
+   }
+   defer f.Close()
+
+   bs := make([]byte, 1024)
+   f.Read(bs)
+   fmt.Println(string(bs))
+}
