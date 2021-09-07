637749dc79256e954a517d6b6d5db6990fa1a627
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-09-07 17:29:02 +0900

    Update null.c

--- Go言語/Goの文法/NULL-nil/null.c
+++ Go言語/Goの文法/NULL-nil/null.c
@@ -3,14 +3,15 @@
 int main(void){
    
    int i;
-   printf("%d\n", i);     // 1
+   printf("%d\n", i);      // 364556325 適当な数字が出てくる
+   printf("%d\n", i + 1);  // 364556326
    
    int *ii;
-   printf("%p\n", ii);    //  0x7ffee0eb2780
-   printf("%d\n", *ii);   // 0
+   printf("%p\n", ii);     //  0x7ffee0eb2780
+   printf("%d\n", *ii);    // 0
 
    int *iii=NULL;
-   printf("%p\n", iii);   // 0x0
-   printf("%d\n", *iii);  // Segmentation fault: 11
+   printf("%p\n", iii);    // 0x0
+   printf("%d\n", *iii);   // Segmentation fault: 11
    
 }
