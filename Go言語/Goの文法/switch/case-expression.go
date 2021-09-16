d7edb3b5be587dc80ad3dd6e0510e150c307d189
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-09-16 19:37:32 +0900

    Create case-expression.go

---
+++ Go言語/Goの文法/switch/case-expression.go
@@ -0,0 +1,19 @@
+/*
+caseに式が書ける。その場合はswitchの中には式は書かない 
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	day := 5
+	switch {
+	case 0 > day && day < 3:
+		fmt.Println("0 < day < 3")
+	case day > 3 && day < 6:
+		fmt.Println("3 < day < 6")
+	}
+}
