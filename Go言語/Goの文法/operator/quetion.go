bc268e081829e921c2a36263d01fb4c2e848824f
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-09-17 14:57:00 +0900

    Create quetion.go

---
+++ Go言語/Goの文法/operator/quetion.go
@@ -0,0 +1,17 @@
+package main
+ 
+import (
+   "fmt"
+)
+
+func main(){
+  fmt.Println(12/5)
+  fmt.Println(12%5)
+  
+	fmt.Println(6<<1)
+	fmt.Println(6<<2)
+	fmt.Println(6<<3)
+
+	fmt.Println(11>>2)
+	fmt.Println(14>>3)
+}
