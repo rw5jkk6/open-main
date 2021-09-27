a01041de43a394523ee2027133d34d7acb7ca17d
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-09-27 16:01:53 +0900

    Update main.go

--- Go言語/Goのアプリ/time/main.go
+++ Go言語/Goのアプリ/time/main.go
@@ -13,10 +13,11 @@ func main(){
    flag.Parse()
 
    rand.Seed(time.Now().UnixNano())
-   r := rand.Intn(10)
+   r := rand.Intn(5)
    t := time.Now()
    t1 := t.AddDate(r, 0, 0)
    fmt.Printf("%sはせりにゃんと%d年に付き合って\n", name, t1.Year())
    t2 := t1.AddDate(1, 2, 0)
+   time.Sleep(3 * time.Second)
    fmt.Printf("%d年%d月に結婚する\n", t2.Year(), t2.Month())
 }
