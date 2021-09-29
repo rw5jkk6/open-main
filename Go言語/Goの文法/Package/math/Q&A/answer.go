541c101a585acb00bd3f58a019343a0679cb3bc3
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-09-29 15:49:51 +0900

    Create answer.go

---
+++ Go言語/Goの文法/Package/math/Q&A/answer.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+   fmt.Println(math.Pi * 4.0)
+   fmt.Println(math.Pow(2, 32))
+   fmt.Println(math.Sqrt(2))
+   fmt.Println(math.Max(3, 2))
+   fmt.Println(math.Ceil(4.1))
+}
