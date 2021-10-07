b5fabb84ecb03ace6556f06cf36c8819e755c486
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-10-07 19:00:18 +0900

    Create 2-panic.go

---
+++ Go言語/Goの文法/nil/2-panic.go
@@ -0,0 +1,13 @@
+package main
+ 
+import (
+   "fmt"
+)
+
+func main(){
+   var i *int
+   fmt.Println(i) // <nil>
+
+   fmt.Println(*i) // panicを起こす
+   // panic: runtime error: invalid memory address or nil pointer dereference
+}
