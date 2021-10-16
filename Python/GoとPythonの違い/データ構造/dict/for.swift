6ba458a12f29eee31ec5898bbf20b8423fc0d5fb
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2021-10-16 09:54:46 +0900

    Create for.swift

---
+++ Python/GoとPythonの違い/データ構造/dict/for.swift
@@ -0,0 +1,17 @@
+var d = ["apple": 2, "banana":3, "orange": 1]
+
+for k1 in d.keys{
+    print(k1)
+}
+
+print("***************")
+
+for k2 in d{
+    print(k2)
+}
+
+print("***************")
+
+for k3 in d.values{
+    print(k3)
+}
