6889100edb7b910a2a18a851ae14cf8df3880794
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2022-06-07 17:03:39 +0900

    Delete A_Tour_of_Go/高効率言語Rust/05_fizzbuzz/複数ファイル/fizzbuzz_python directory

--- A_Tour_of_Go/高効率言語Rust/05_fizzbuzz/複数ファイル/fizzbuzz_python/test_app.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from app import fizzbuzz
-
-class TestFizzbuzz(unittest.TestCase):
-    def testfizzbuzz(self):
-        self.assertEqual(fizzbuzz(15), "fizzbuzz")
-        self.assertEqual(fizzbuzz(3), "fizz")
