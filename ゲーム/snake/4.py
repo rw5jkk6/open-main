69b63aa0b7cfde18304acf62991ceb3a493c0886
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2022-06-08 15:59:24 +0900

    Create 4.py

---
+++ ゲーム/snake/4.py
@@ -0,0 +1,61 @@
+import sys
+import pygame
+from pygame.locals import QUIT, Rect, KEYDOWN,K_LEFT,K_RIGHT,K_UP,K_DOWN
+
+pygame.init()
+SURFACE = pygame.display.set_mode((600,600))
+FPSCLOCK = pygame.time.Clock()
+
+(W,H)=(20,20)
+SNAKE = []
+
+def paint():
+    SURFACE.fill((0,0,0))
+    for body in SNAKE:
+        pygame.draw.rect(SURFACE, (0, 255, 255), Rect(body[0]*30, body[1]*30, 30, 30))
+
+    for index in range(20):
+        # 縦線が描かれる
+        pygame.draw.line(SURFACE, (64, 64, 64), (index*30, 0),(index*30, 600))
+        # 横線が描かれる
+        pygame.draw.line(SURFACE, (64, 64, 64), (0, index*30),(600, index*30))
+
+    pygame.display.update()
+
+def main():
+    key = K_DOWN
+    game_over = False
+    SNAKE.append((int(W/2),int(H/2)))
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                key = event.key
+        
+        if not game_over:
+            if key == K_LEFT:
+                head = (SNAKE[0][0]-1, SNAKE[0][1])
+            elif key == K_RIGHT:
+                head = (SNAKE[0][0]+1, SNAKE[0][1])
+            elif key == K_UP:
+                head = (SNAKE[0][0], SNAKE[0][1]-1)
+            elif key == K_DOWN:
+                head = (SNAKE[0][0], SNAKE[0][1]+1)
+        
+            if head in SNAKE or \
+                head[0] < 0 or \
+                head[0] >= W or \
+                head[1] < 0 or \
+                head[1] >= H :
+                game_over = True
+
+        SNAKE.insert(0, head)
+        SNAKE.pop()
+
+        paint()
+        FPSCLOCK.tick(5)
+
+if __name__ == "__main__":
+    main()
