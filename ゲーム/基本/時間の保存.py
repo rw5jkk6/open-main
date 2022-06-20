1942b85854243fafe4dc02326c0b99ec30a1cbb8
rw5jkk6 <66894552+rw5jkk6@users.noreply.github.com>
2022-06-20 15:48:00 +0900

    Create 時間の保存.py

---
+++ ゲーム/基本/時間の保存.py
@@ -0,0 +1,32 @@
+import sys, datetime
+from termios import PARODD
+import pygame
+from pygame.locals import QUIT
+
+def tick():
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            t = datetime.datetime.now()
+            with open("time.txt", "a") as f:
+                f.write(str(t))
+                f.write("\n")
+            pygame.quit()
+            sys.exit()
+
+pygame.init()
+pygame.key.set_repeat(5,5)
+SURFACE=pygame.display.set_mode((600,800))
+FPSCLOCK=pygame.time.Clock()
+
+def main():
+    fps = 30
+    while True:
+        tick()
+
+        SURFACE.fill((0,0,0))
+
+        pygame.display.update()
+        FPSCLOCK.tick(fps)
+
+if __name__ == "__main__":
+    main()
